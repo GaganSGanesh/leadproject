52b7e2f3d60a45039511866b3176e56194a2dbdf
GaganSg1 <gaganqsp@gmail.com>
2021-11-28 12:22:32 +0530

    PUSHING MODIFIED DATA AS TE

---
+++ src/leadprojectpack1/modifiedasTE.java
@@ -0,0 +1,8 @@
+package leadprojectpack1;
+
+public class modifiedasTE {
+	public static void main(String[] args) {
+		System.out.println("modified as test engineer!!!");
+	}
+
+}
---
+++ src/leadprojectpack1/modifiedasTE2.java
@@ -0,0 +1,8 @@
+package leadprojectpack1;
+
+public class modifiedasTE2 {
+	public static void main(String[] args) {
+		System.out.println("modified as test engineer-------------------->!!!");
+	}
+
+}
